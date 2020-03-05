b974d09abb17c609117132c7e138a0585be00232
Matias Duarte <33585271+mnduarte@users.noreply.github.com>
2020-03-05 12:54:56 -0300

    FalconSoft Exercise - React

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Suspense } from 'react';
+import './styles/App.scss';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import Loading from './components/Loading';
+import Layout from './containers/Layout';
+const App = () => {
+  return (
+    <HashRouter>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route 
+            path="/"
+            name="Home"
+            render={props => <Layout {...props} />}
+          />
+        </Switch>
+      </Suspense>
+    </HashRouter>
+  ); 
+}
+
+export default App;
---
+++ src/views/TablePage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { ColumnDirective, ColumnsDirective, Filter, GridComponent, Group, Search, Toolbar } from '@syncfusion/ej2-react-grids';
+import { Inject, Page, Sort } from '@syncfusion/ej2-react-grids';
+import {data} from './datasource';
+
+const TablePage = () => {
+  const pageSettings = { pageSize: 20 }
+  const sortSettings = { columns: [
+                          {field: 'EmployeeID', direction: 'Ascending' }
+                      ] };
+  const toolbarOptions = ['Search'];
+  return (
+    <>
+      <section>
+        <h1>Table Page</h1>
+        <GridComponent dataSource={data} allowPaging={true} pageSettings={ pageSettings }
+            allowSorting={true} sortSettings={ sortSettings }  toolbar={toolbarOptions}>
+            <ColumnsDirective>
+                <ColumnDirective field='EmployeeID' width='100'/>
+                <ColumnDirective field='OrderID' width='100' />
+                <ColumnDirective field='CustomerID' width='100'/>
+                <ColumnDirective field='Freight' width='100' format="C2"/>
+                <ColumnDirective field='ShipCountry' width='100'/>
+            </ColumnsDirective>
+            <Inject services={[Page, Sort, Filter, Group, Search, Toolbar]} />
+        </GridComponent>
+      </section>
+    </>
+  );
+};
+
+export default TablePage;
